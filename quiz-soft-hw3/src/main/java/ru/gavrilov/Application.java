0d3668d55e6bce14446fcb181fc75fbe810c19b0
serj <serj.g13@yandex.ru>
2019-03-18 17:19:51 +0300

    HW-3: Убрал имплементацию интерфейса CommandLineRunner.

--- quiz-soft-hw3/src/main/java/ru/gavrilov/Application.java
+++ quiz-soft-hw3/src/main/java/ru/gavrilov/Application.java
@@ -1,27 +1,13 @@
 package ru.gavrilov;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.gavrilov.presentation.PresenterQuizService;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    private final PresenterQuizService presenterQuizService;
-
-    @Autowired
-    public Application(PresenterQuizService presenterQuizService) {
-        this.presenterQuizService = presenterQuizService;
-    }
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        presenterQuizService.runTest();
+        SpringApplication.run(Application.class, args).getBean(PresenterQuizService.class).runTest();
     }
 }
