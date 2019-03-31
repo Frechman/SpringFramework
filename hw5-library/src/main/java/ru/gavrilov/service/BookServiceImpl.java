479557e00af948a4c258e57093dae8a395cc7b60
Frechman <31288209+Frechman@users.noreply.github.com>
2019-03-31 19:59:53 +0300

    HW-5: Add services.

--- hw5-library/src/main/java/ru/gavrilov/service/BookServiceImpl.java
+++ hw5-library/src/main/java/ru/gavrilov/service/BookServiceImpl.java
@@ -1,4 +1,71 @@
 package ru.gavrilov.service;
 
-public class BookServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.gavrilov.dao.BookRepository;
+import ru.gavrilov.model.Book;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book findByIsbn(String isbn) {
+        if (isbn != null) {
+            return bookRepository.findByIsbn(isbn);
+        }
+        throw new IllegalArgumentException("Isbn is not null!");
+    }
+
+    @Override
+    public void insert(Book book) {
+        if (book != null) {
+            bookRepository.insert(book);
+        }
+        throw new IllegalArgumentException("Book is not null!");
+    }
+
+    @Override
+    public void deleteByIsbn(String isbn) {
+        if (isbn != null) {
+            bookRepository.deleteByIsbn(isbn);
+        }
+        throw new IllegalArgumentException("Isbn is not null!");
+    }
+
+    @Override
+    public List<Book> findAllByGenre(String genreName) {
+        if (genreName != null) {
+            return bookRepository.findAllByGenre(genreName);
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Book> findAllByAuthor(String authorLastName) {
+        if (authorLastName != null) {
+            return bookRepository.findAllByAuthor(authorLastName);
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public int count() {
+        return bookRepository.count();
+    }
+
 }
