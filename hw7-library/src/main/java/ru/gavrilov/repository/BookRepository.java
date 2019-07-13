2096e79ae93311367d4854c4368f28f9ef2de923
Frechman <31288209+Frechman@users.noreply.github.com>
2019-07-13 22:37:31 +0300

    HW-7: Убрал проблему n+1. Переписал тесты BookService

--- hw7-library/src/main/java/ru/gavrilov/repository/BookRepository.java
+++ hw7-library/src/main/java/ru/gavrilov/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package ru.gavrilov.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.gavrilov.model.Book;
 
@@ -9,6 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+
+    @Query("select b from Book b join fetch b.author a join fetch b.genre g")
+    List<Book> findAll();
 
     Optional<Book> findByIsbn(String isbn);
 
