cf123995c3875e575e6a1762db53e9491ca7bc64
Frechman <31288209+Frechman@users.noreply.github.com>
2019-09-03 09:18:36 +0300

    hw-9: Tests for books controllers.

--- hw-9-library-mvc/src/test/java/ru/gavrilov/controllers/BookControllerTest.java
+++ hw-9-library-mvc/src/test/java/ru/gavrilov/controllers/BookControllerTest.java
@@ -2,6 +2,7 @@ package ru.gavrilov.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,10 +23,10 @@ import java.util.Random;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = BookController.class)
+@DisplayName("Book controller's methods should ")
 class BookControllerTest {
 
     @Autowired
@@ -66,13 +67,17 @@ class BookControllerTest {
     }
 
     @Test
+    @DisplayName("return model with 'bookList' attribute " +
+            "and return view 'listBooks' when a GET request was made for '/books'")
     void testListBook() throws Exception {
         when(bookService.findAll()).thenReturn(bookList);
 
         mockMvc.perform(get("/books"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
-                //// TODO: 02.09.2019 draft
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attribute("books", bookList))
+                .andExpect(view().name("listBooks"))
                 .andExpect(content().string(containsString("123-456")))
                 .andExpect(content().string(containsString("999-888")));
 
@@ -81,20 +86,24 @@ class BookControllerTest {
     }
 
     @Test
+    @DisplayName("return model with 'book','authorList','genreList' attributes" +
+            "and return view 'addBook' when a GET request was made for '/books/add'")
     void testAddBook() throws Exception {
-        when(bookService.findAll()).thenReturn(bookList);
-        when(authorService.findAll()).thenReturn(Arrays.asList(new Author("123", "123"), new Author("321", "321")));
-        when(genreService.findAll()).thenReturn(Arrays.asList(
+        List<Author> expectedAuthorList = Arrays.asList(new Author("123", "123"), new Author("321", "321"));
+        when(authorService.findAll()).thenReturn(expectedAuthorList);
+        List<Genre> expectedGenreList = Arrays.asList(
                 new Genre("name1", "desc1"),
-                new Genre("name2", "desc2"))
-        );
+                new Genre("name2", "desc2"));
+        when(genreService.findAll()).thenReturn(expectedGenreList);
 
-        mockMvc.perform(get("/books/add")
-                .content(objectMapper.writeValueAsBytes(bookList))
-        )
+        mockMvc.perform(get("/books/add"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"))
-                //// TODO: 02.09.2019 draft
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attribute("book", new Book()))
+                .andExpect(model().attribute("authorList", expectedAuthorList))
+                .andExpect(model().attribute("genreList", expectedGenreList))
+                .andExpect(view().name("addBook"))
                 .andExpect(content().string(containsString("123 123")))
                 .andExpect(content().string(containsString("321 321")))
                 .andExpect(content().string(containsString("name1")))
