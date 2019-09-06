d6031f247d7bccd600c020a7ea803ae356af8589
Frechman <31288209+Frechman@users.noreply.github.com>
2019-09-06 15:41:28 +0300

    hw-10: Tuned webpack config and updated node,npm. Added bootstrap, etc.

--- hw10-library-react/library-react/webpack.config.js
+++ hw10-library-react/library-react/webpack.config.js
@@ -12,12 +12,17 @@ module.exports = {
     module: {
         rules: [
             {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
+                test: /\.js$/, exclude: /node_modules/,
                 use: [{
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            {
+                                "plugins": ["@babel/plugin-proposal-class-properties"]
+                            }
+                        ]
                     }
                 }]
             }
